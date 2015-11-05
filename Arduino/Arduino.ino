888ebe20175cefb22ac1017acfa5d4ce5b7d9a6a
joshuagruenstein <joshuagruenstein@gmail.com>
2015-11-05 11:37:42 -0500

    stuff's not working
    I’m testing without the motor shield and with software serial.
    esp8266.begin() works but connect() does not, no matter what network I
    set it to.  gives error -2.

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,16 +1,42 @@
+#include <SoftwareSerial.h>
+#include <SparkFunESP8266WiFi.h>
+
 #define LEFT 1
 #define RIGHT 0
 
+bool good = false;
+ESP8266Client client;
+bool state = false;
+
 void setup() {  
+	Serial.begin(9600);
+	
+	pinMode(13,OUTPUT);
+
+	if (esp8266.begin()) {
+		Serial.println("We're good");
+	} else {
+		Serial.println("We're not");
+	}
+	
+	int retVal = esp8266.connect("academic","");
+	if (retVal < 0) {
+		Serial.print(F("Error connecting: "));
+		Serial.println(retVal);
+    } else {
+		Serial.println("Stuff is good");
+	}
+
+	Serial.print(F("My IP is: ")); Serial.println(esp8266.localIP());
+
+	retVal = client.connect("192.168.173.1",2000);
+	if (retVal<=0) return;
+
+	good = true;
 	motorInit();
 }
 
 void loop() {
-	setMotor(LEFT,100);
-	setMotor(RIGHT,100);
-	delay(1000); 
-
-	setMotor(LEFT,100);
-	setMotor(RIGHT,-100);
-	delay(400);
+	while (client.available())
+    	client.print(client.read());
 }
